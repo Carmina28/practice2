65fd46263f0f1b7ad76856d2d10739a99d495192
Carmina <20ecar.race02@gmail.com>
2023-02-03 22:33:05 -0600

    adding concepts two

---
+++ main2.py
@@ -0,0 +1,19 @@
+#variables
+age = 15
+name = "Maria"
+mayorEdad = "Es mayor de edad"
+menorEdad = "Es menor de edad"
+
+#funciones
+
+def main():
+    print("hola")
+
+def main_method():
+
+    edad = int(input("Que edad tienes?"))
+
+    if (edad>18):
+        print(mayorEdad)
+    else:
+        print(menorEdad)
